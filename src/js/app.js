972dbc572cf0c31b447d17181230f79fd3dcad99
Sahwag546 <sahwagkantiwal494@gmail.com>
2024-09-26 11:55:09 +0530

    add slider

--- src/js/app.js
+++ src/js/app.js
@@ -1,29 +1,72 @@
-$('.third-slider').slick({
-    dots: true,
-    arrow: false,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplaySpeed: 3000,
-    centermode: true,
-    prevArrow: $('.prev'),
-    nextArrow: $('.next'),
-     
-    responsive: [
 
-        {
-            breakpoint: 992,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
-            }
-        },
-        {
-            breakpoint: 786,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        }
-    ]
+        $('.show-three').slick({
+            dots: false,
+            arrow: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            autoplaySpeed: 3000,
+            prevArrow: $('.prev'),
+            nextArrow: $('.next'),
+
+            responsive: [
+                {
+                    breakpoint: 1300,
+                    settings: {
+                        slidesToShow: 6,
+                        slidesToScroll: 2
+                    }
+                },
+
+                {
+                    breakpoint: 1200,
+                    settings: {
+                        slidesToShow: 5,
+                        slidesToScroll: 2
+                    }
+                },
+                {
+                    breakpoint: 992,
+                    settings: {
+                        slidesToShow: 4,
+                        slidesToScroll: 1
+                    }
+                },
+                {
+                    breakpoint: 786,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 1
+                    }
+                },
+                  {
+                    breakpoint: 640,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1
+                    }
+                },
+                {
+                    breakpoint: 576,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1
+                    }
+                },
+                 {
+                    breakpoint: 440,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                },
+                {
+                    breakpoint: 390,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
+
+        });
     
-});
